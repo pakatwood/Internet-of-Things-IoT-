a52c35d00e2093fdcde2d82131c9e9d5c6a631cd
Haider Qazi <68088522+pakatwood@users.noreply.github.com>
2020-12-22 16:03:45 -0600

    Motor Movement

---
+++ Motor Movement/motor_movement/motor_movement.ino
@@ -0,0 +1,13 @@
+void setup(){
+  Robot.begin();
+}
+void loop(){
+  Robot.motorsWrite(255,255); //Make the robot go forward, full speed
+  delay(1000);
+  Robot.motorsWrite(0,0); //Make the robot stop
+  delay(1000);
+  Robot.motorsWrite(255,-255);//Make the robot rotate right, full speed
+  delay(1000);
+  Robot.motorsWrite(0,0); //Make the robot stop
+  delay(1000);
+}
